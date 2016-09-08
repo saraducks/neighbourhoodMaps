79892d11137841bdc4825e79bfe3de95b29177be
saraducks <saranyadhakshina25@gmail.com>
2016-09-08 11:51:37 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,90 @@
-
-      function initMap() {
-        var latlng = {lat: 40.7413549, lng: -73.9980244};
+\     function initMap() {
+       //styles the map
+       var stylesArray = [
+            {
+              featureType: 'water',
+              stylers: [
+                { color : "#adc9b8"}
+              ]
+            },
+            {
+              featureType: 'landscape',
+              stylers:[
+                {saturation:-100},
+                {lightness:65},
+                {visibility: "on"}
+            ]
+           },
+           {
+            featureType: 'road.local',
+            stylers:[
+               {saturation:-100},
+               {lightness:80},
+               {color : "#809f80"},
+               {visibility:"on"}
+          ]
+        }
+       ]
+       var markers = [];
+       //setting up the places I want to visit
+       var locations = [
+        {title : 'pho lovers',location : {lat:37.368830,lng:-122.036350}},
+        {title: 'santana row',location: {lat:37.321632,lng:-121.947919}},
+        {title: 'westfield mall',location:{lat:37.305210,lng:-121.945184}}
+        ];
+        
+        // information window of the marker         
+        var infowindow = new google.maps.InfoWindow();
+        
+        //assign different colors on mouse over and mouse off
+        var mouseoff = MarkerImage('#2c7fb8');
         // Constructor creates a new map - only center and zoom are required.
         var map = new google.maps.Map(document.getElementById('map'),{
-          center: latlng,
-          zoom: 4
-        });
-        //adding the marker to the location specified
-         var marker = new google.maps.Marker({
-               position: latlng,
-               map: map,
-               title: 'Hello World!'
-        });
-        //displays the info window to the marker
-        var infowindow = new google.maps.InfoWindow({
-          content : 'This is my first infowindow display'
-        });
+          center: {lat:37.369008,lng:-122.038652},
+          animation : google.maps.Animation.Drop,
+          zoom: 13,
+          styles: stylesArray,
+          mapTypeControl : false
+          });
+
+        // create the marker for each location
+        for(var i=0; i < locations.length; i++){
+           var title = locations[i].title;
+           var position = locations[i].location;
+           var image = new google.maps.MarkerImage(
+            "https://tse4.mm.bing.net/th?id=OIP.M1f8aad22ca89d9d4e6dc452e8af595d4o0",
+            null,
+            null,
+            null,
+            new google.maps.Size(50,50)
+            )
+           var icon = image;
+           var marker = new google.maps.Marker({
+                 position: position,
+                 map: map,
+                 title: title,
+                 icon : icon
+           });
+
+        //adds the marker to the marker array
+        markers.push(marker);
+        
         marker.addListener('click',function(){
-          infowindow.open(map,marker);
+          populateInfoWindow(this,infowindow);
         });
-      }
+       }
+     }
+        //populate contents to the infowindow
+        function populateInfoWindow(marker,infowindow){
+          if(infowindow.marker != marker){
+            infowindow.marker = marker;
+            infowindow.setContent('<div>'+marker.position+'</div>');
+            infowindow.open(map,marker);
+
+            infowindow.addListener('close',function(){
+              infowindow.setMarker(null);
+            });
+          }
+        }
+       
+  
